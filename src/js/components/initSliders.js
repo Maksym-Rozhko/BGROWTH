08ef674bbaec4a03ae7619a9059a84f1dd1966b5
Maksym <maksymRozhko7@gmail.com>
2023-04-07 11:55:56 +0300

    section point

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -1,13 +1,20 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
-new Swiper('.swiper', {
-	slidesPerView: 4,
+const pointSlider = new Swiper('.swiper.point-slider', {
+	slidesPerView: 2,
 	spaceBetween: 20,
+    speed: 1000,
 
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
+            spaceBetween: 10,
 		},
+        768: {
+			slidesPerView: 2,
+            spaceBetween: 20,
+        }
 	},
 });
+
